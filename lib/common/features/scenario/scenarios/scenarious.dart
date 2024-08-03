e68b7586c681d542a7cb2c678242711c55ae8e45
madbers4 <antonantion@gmail.com>
2024-08-03 17:41:39 +0300

    DEBATE-35

--- lib/common/features/scenario/scenarios/scenarious.dart
+++ lib/common/features/scenario/scenarios/scenarious.dart
@@ -1,5 +1,5 @@
 import 'package:v1/common/features/scenario/Scenario.dart';
-import 'package:v1/common/features/scenario/scenarios/strangeEventsAtTheLucerneCafe/strangeEventsAtTheLucerneCafe.dart';
+import 'package:v1/common/features/scenario/scenarios/strange-events-at-the-lucerne-cafe/strange-events-at-the-lucerne-cafe.dart';
 import 'package:v1/common/features/scenario/scenarios/test/test.dart';
 
 final List<Scenario> scenarious = [strangeEventsAtTheLucerneCafe, testScenario];
